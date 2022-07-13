892000b8f2005d272304209dd36a254c2f41af10
unknown <manuelgamez92@gmail.com>
2022-07-13 13:39:30 -0700

    String error fixed

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -10,7 +10,7 @@ export default function signin({providers}) {
             <div key={provider.name} className="">
             <img src="https://1000marcas.net/wp-content/uploads/2020/02/logo-Google.png" alt="google logo" className="w-52 object-cover"></img>
             <p className='text-sm italic my-10 text-center'>This site is created for learning purposes</p>
-            <button className="bg-red-600 rounded-lg text-white p-3 hover:bg-red-700" onClick={()=>signIn(provider.id, {callbarckUrl: "/" })}>Sign in with {provider.name}</button>
+            <button className="bg-red-600 rounded-lg text-white p-3 hover:bg-red-700" onClick={()=>signIn(provider.id, {callbackUrl: "/" })}>Sign in with {provider.name}</button>
             </div>
           ))}
         </div>
